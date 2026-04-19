befc38077638a1dca47fd051a49cdd4b17a17910
Randy Mendez <randy.mendez@alumnos.upm.es>
2026-04-20 01:03:46 +0200

    add engagement web client #15

---
+++ src/main/java/es/upm/api/domain/webclients/EngagementWebClient.java
@@ -0,0 +1,20 @@
+package es.upm.api.domain.webclients;
+
+import es.upm.api.configurations.FeignConfig;
+import es.upm.api.domain.model.platform.EngagementLetterSummary;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "goa-engagement",
+        url = "${goa.engagement.base-url}",
+        configuration = FeignConfig.class
+)
+public interface EngagementWebClient {
+    String ENGAGEMENT_LETTERS = "/engagement-letters";
+    String ID_ID = "/{id}";
+
+    @GetMapping(ENGAGEMENT_LETTERS + ID_ID)
+    EngagementLetterSummary readById(@PathVariable String id);
+}
