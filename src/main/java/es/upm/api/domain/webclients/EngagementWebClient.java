50cdc8f3bcc7cdf6b607b5c09dd0ac72b003041f
Randy Mendez <randy.mendez@alumnos.upm.es>
2026-04-20 02:09:07 +0200

    add engagement events client #15

--- src/main/java/es/upm/api/domain/webclients/EngagementWebClient.java
+++ src/main/java/es/upm/api/domain/webclients/EngagementWebClient.java
@@ -1,10 +1,12 @@
 package es.upm.api.domain.webclients;
 
 import es.upm.api.configurations.FeignConfig;
+import es.upm.api.domain.model.platform.EngagementEventPage;
 import es.upm.api.domain.model.platform.EngagementLetterSummary;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "goa-engagement",
@@ -14,7 +16,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface EngagementWebClient {
     String ENGAGEMENT_LETTERS = "/engagement-letters";
     String ID_ID = "/{id}";
+    String EVENTS = "/events";
 
     @GetMapping(ENGAGEMENT_LETTERS + ID_ID)
     EngagementLetterSummary readById(@PathVariable String id);
+
+    @GetMapping(ENGAGEMENT_LETTERS + ID_ID + EVENTS)
+    EngagementEventPage readEventsByEngagementLetterId(
+            @PathVariable String id,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    );
 }
